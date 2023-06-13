e981c376067b888e723b07a9b92ab2ad81353a87
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-06-13 11:59:53 -0500

    11 commit

--- Blogspot/blog/views.py
+++ Blogspot/blog/views.py
@@ -3,4 +3,6 @@ from .models import Post
 from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    for post in posts:
+        post.publish()
     return render(request, 'blog/post_list.html', {'posts': posts})

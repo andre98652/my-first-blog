be42623b1da529913bebadf78c9b536e7319ce81
Andre Delgado <adelgadoal@unsa.edu.pe>
2023-06-13 12:40:10 -0500

    11 commit

--- Blogspot/blog/views.py
+++ Blogspot/blog/views.py
@@ -5,4 +5,5 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     for post in posts:
         post.publish()
+        post.save() 
     return render(request, 'blog/post_list.html', {'posts': posts})
